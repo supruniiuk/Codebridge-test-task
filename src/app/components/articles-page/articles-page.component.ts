97a24981eee66f4e3857f88e903aebcc4832ea96
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-05-24 16:13:59 +0300

    added loading from server + pagination

--- src/app/components/articles-page/articles-page.component.ts
+++ src/app/components/articles-page/articles-page.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+import { ArticleService } from 'src/app/services/article.service';
+import { Article } from 'src/app/shared/interfaces/article.interface';
 
 @Component({
   selector: 'app-articles-page',
   templateUrl: './articles-page.component.html',
   styleUrls: ['./articles-page.component.scss'],
 })
-export class ArticlesPageComponent {
+export class ArticlesPageComponent implements OnInit {
+  subs: Subscription[] = [];
+  articles: Article[] = [];
+
+  currentPage: number = 1;
+  limit: number = 10;
+  totalArticles: number
+
+  constructor(private aricleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.getArticleCounter();
+    this.getAllArticles();
+  }
+
+  getAllArticles() {
+    const articleSubscription = this.aricleService
+      .getArticles(this.limit, this.currentPage)
+      .subscribe((res) => {
+        this.articles = res;
+      });
+
+    this.subs.push(articleSubscription)
+  }
+
+  getArticleCounter() {
+    const countSubscription = this.aricleService.getArticlesCount().subscribe((res) => {
+      this.totalArticles = +res;
+    })
+
+    this.subs.push(countSubscription)
+  }
+
+  handlePage(e: PageEvent) {
+    this.currentPage = e.pageIndex;
+    this.limit = e.pageSize;
+    this.getAllArticles()
+  }
+  
+  ngOnDestroy() {
+    this.subs.forEach((sub) => sub.unsubscribe());
+  }
 }
