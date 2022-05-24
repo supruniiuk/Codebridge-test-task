d65a87c7664281c8e7d0d156acf4239d960d7b3f
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-05-24 16:12:32 +0300

    added loading from server

--- src/app/components/article-info/article-info.component.ts
+++ src/app/components/article-info/article-info.component.ts
@@ -5,7 +5,10 @@ import {
   Renderer2,
   ViewChild,
 } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ArticleService } from 'src/app/services/article.service';
+import { Article } from 'src/app/shared/interfaces/article.interface';
 
 @Component({
   selector: 'app-article-info',
@@ -13,16 +16,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./article-info.component.scss'],
 })
 export class ArticleInfoComponent implements OnInit {
+  subs: Subscription[] = [];
+  articleId: number;
+  article: Article;
+
   @ViewChild('articleImg') articleImg;
-  constructor(public router: Router, private renderer: Renderer2) {}
+  
+  constructor(public route: ActivatedRoute, private articleService: ArticleService, private renderer: Renderer2) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const routeSubscription = this.route.params.subscribe(params => {
+      this.articleId = +params.id;
+      this.getArticleById(this.articleId)
+    })
 
-  ngAfterViewInit() {
+    this.subs.push(routeSubscription);
+  }
+
+  getArticleById(id: number) {
+    const getArticleSubscription = this.articleService.getArticleById(id).subscribe((res) => {
+      this.article = res;
+      this.setBackground()
+    })
+
+    this.subs.push(getArticleSubscription)
+  }
+
+  setBackground() {
     this.renderer.setStyle(
       this.articleImg.nativeElement,
       'background',
-      'center / cover no-repeat url(https://material.angular.io/assets/img/examples/shiba2.jpg)'
+      `center / cover no-repeat url(${this.article.imageUrl})`
     );
+  }
+
+
+  ngOnDestroy() {
+    this.subs.forEach((sub) => sub.unsubscribe());
   }
 }
